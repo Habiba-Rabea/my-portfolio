66cb79e3cae325948481075af69bd01a604a8b79
Habiba-Rabea <habibarabea205@gmail.com>
2026-09-08 13:01:08 +0200

    some-edits

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/my-portfolio', // اكتب اسم الـ Repository هنا بنفس الحروف exacta
   typescript: {
     ignoreBuildErrors: true,
   },
